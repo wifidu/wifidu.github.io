fc7c29e38826f6ed9aa707024f4b93b0aefe8b80
wifidu <875147715@qq.com>
2020-06-18 13:57:03 +0800

    2020年 06月 18日 星期四 13:57:03 CST-blog

--- test.sh
+++ test.sh
@@ -12,13 +12,12 @@ function you() {
   printf "[%-25s][%d%%][%c]\r" $str $i ${ch[$index]}
 }
 
-printf "123"
-# while [ $i -le 25 ]
-# do
-#     printf "[%-25s][%d%%][%c]\r" $str $(($i*4)) ${ch[$index]}
-#     str+='#'
-#     let i++
-#     let index=i%4
-#     sleep 0.1
-# done
-# printf "\n"
+while [ $i -le 25 ]
+do
+    printf "[%-25s][%d%%][%c]\r" $str $(($i*4)) ${ch[$index]}
+    str+='#'
+    let i++
+    let index=i%4
+    sleep 0.1
+done
+printf "\n"
