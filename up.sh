d05dc8ea98ac1e1527fc25b4bcabde20f214ad07
wifidu <875147715@qq.com>
2020-06-18 13:32:23 +0800

    2020年 06月 18日 星期四 13:32:23 CST-blog

--- up.sh
+++ up.sh
@@ -9,19 +9,19 @@ function jindu() {
   printf "progress:[%-50s]%d%%\r" $b $i
 }
 
-echo "blog updating==>>>>>"
+echo "blog updating[===========]0%"
 hexo g -d > /dev/null 2>&1
 git add -A > /dev/null 2>&1
 git commit -m "$(date)-blog" > /dev/null 2>&1
 git push > /dev/null 2>&1
-echo "blog updated successfully"
+echo "blog updated successfully[>>>>>=======]50%"
 
 cd ~/note
 git add -A > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "note nothing to update"
 else
-    echo -e "\nnote updating==>>>>>"
+    echo -e "\nnote updating[>>>>>>>>>>]100%"
     git commit -m "$(date)" > /dev/null 2>&1
     git push > /dev/null 2>&1
     echo "note updated successfully"
