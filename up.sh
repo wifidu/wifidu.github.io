4c23512a91c9730e4522966ef86909e4610ea74f
wifidu <875147715@qq.com>
2020-06-18 13:13:07 +0800

    2020年 06月 18日 星期四 13:13:07 CST-blog

--- up.sh
+++ up.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+echo "blog updating==>>>>>"
 hexo g -d > /dev/null 2>&1
 git add -A > /dev/null 2>&1
 git commit -m "$(date)-blog" > /dev/null 2>&1
 git push > /dev/null 2>&1
-
 echo "blog updated successfully"
 
 cd ~/note
@@ -12,9 +12,9 @@ git add -A > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "note nothing to update"
 else
-    git commit -m $(date)
-    git push
-
+    echo "\n blog updating"
+    git commit -m "$(date)"
+    git push > /dev/null 2>&1
     echo "note updated successfully"
 fi
 
