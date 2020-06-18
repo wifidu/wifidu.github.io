231ab8f28e1cf055a0042247b852986ac55be452
wifidu <875147715@qq.com>
2020-06-18 13:47:05 +0800

    2020年 06月 18日 星期四 13:47:05 CST-blog

--- up.sh
+++ up.sh
@@ -15,25 +15,25 @@ function you() {
 
 
 you
-echo "blog updating[===========]0%"
+# echo "blog updating[===========]0%"
 hexo g -d > /dev/null 2>&1
 git add -A > /dev/null 2>&1
 git commit -m "$(date)-blog" > /dev/null 2>&1
 git push > /dev/null 2>&1
 you
-echo "blog updated successfully[>>>>>=======]50%"
+# echo "blog updated successfully[>>>>>=======]50%"
 
 you
 cd ~/note
 git add -A > /dev/null 2>&1
 you
 if [ $? -ne 0 ]; then
-    echo "note nothing to update"
+    # echo "note nothing to update"
 else
-    echo -e "\nnote updating[>>>>>>>>==]80%"
+    # echo -e "\nnote updating[>>>>>>>>==]80%"
     git commit -m "$(date)" > /dev/null 2>&1
     git push > /dev/null 2>&1
-    echo "note updated successfully[>>>>>>>>>>]100%"
+    # echo "note updated successfully[>>>>>>>>>>]100%"
 you
 fi
 
